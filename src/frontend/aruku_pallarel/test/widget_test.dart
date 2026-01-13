db88e1f46822cfc68fcb725d7804a0689dae046b
bo_neko <52018660+i-icc@users.noreply.github.com>
2026-01-13 19:48:22 +0900

    Fix Flutter pub resolution and tests

--- src/frontend/aruku_pallarel/test/widget_test.dart
+++ src/frontend/aruku_pallarel/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:aruku_pallarel/main.dart';
+import 'package:aruku_pallarel/screens/auth/login_screen.dart';
 
 void main() {
   testWidgets('Login screen renders fields', (WidgetTester tester) async {
